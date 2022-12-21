8d44e4df39a81ef435fba10f2779fba0e9aad266
Semyon Zernov <sem.z@protonmail.com>
2022-12-21 20:50:34 +0300

    add spotifywm

--- darkman/.local/share/dark-mode.d/4spotify.sh
+++ darkman/.local/share/dark-mode.d/4spotify.sh
@@ -6,5 +6,5 @@ spicetify update
 if pgrep -x spotify > /dev/null 2>&1 ; then
     killall -q spotify
     while pgrep -x spotify >/dev/null; do sleep 1; done
-    LD_PRELOAD=/usr/lib/spotify-adblock.so spotify &>/dev/null
+    LD_PRELOAD=/usr/lib/spotifywm.so:/usr/lib/spotify-adblock.so /usr/bin/spotify &>/dev/null
 fi
