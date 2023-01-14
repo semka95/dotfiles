909e1ac61d9469c4bb44b25bc9caacf64a3bf507
Semyon Zernov <sem.z@protonmail.com>
2023-01-14 15:53:11 +0300

    fix screenshare script

--- scripts/.local/bin/scripts/screenshare.sh
+++ scripts/.local/bin/scripts/screenshare.sh
@@ -3,8 +3,6 @@
 
 geometry(){
     windowGeometries=$(
-    # `height - 1` is there because of: https://github.com/ammen99/wf-recorder/pull/56 (I could remove it if it's merged, maybe)
-    # swaymsg -t get_workspaces -r | jq -r '.[] | select(.focused) | .rect | "\(.x),\(.y) \(.width)x\(.height - 1)"'; \
         swaymsg -t get_outputs -r | jq -r '.[] | select(.active) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'
     )
     geometry=$(slurp -b "#45858820" -c "#45858880" -w 3 -d <<< "$windowGeometries") || exit $?
@@ -37,20 +35,12 @@ stop_recording() {
             false
         fi
     elif [ "$1" == "start" ]; then
-        VIDEO_DEVICE="/dev/video1"
+        VIDEO_DEVICE="/dev/video0"
         if ! pgrep wf-recorder > /dev/null; then
             geometry=$(geometry) || exit $?
-            wf-recorder --muxer=v4l2 --codec=rawvideo --pixel-format=yuv420p --file="$VIDEO_DEVICE" --geometry="$geometry" &
+            wf-recorder -g "$geometry" --muxer=v4l2 --codec=mjpeg_vaapi -d /dev/dri/renderD128 --file="$VIDEO_DEVICE" &
         fi
-        if ! pgrep ffplay; then
-            unset SDL_VIDEODRIVER
-            ffplay "$VIDEO_DEVICE" -loglevel 24 &
-            sleep 0.5
-            # a hack so FPS is not dropping
-            swaymsg "[class=ffplay]" move workspace screencast
-            swaymsg focus tiling
-        fi
-        if pgrep wf-recorder > /dev/null && pgrep ffplay > /dev/null; then
+        if pgrep wf-recorder > /dev/null; then
             notify-send -t 2000 "Wayland recording has been started"
         else
             stop_recording
